764962026dd6b9bfc1d10a767810db2fae93bfaf
Cameron Zipp <Zipp.Cameron@student.greenriver.edu>
2022-01-31 10:14:29 -0500

    Creating Fat-free framework

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+
+//turn on error reporting
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+//require autoload file
+require_once('vendor/autoload.php');
+
+//create instance of the base class
+$f3 = Base::instance();
+
+//define default route
+$f3->route('GET /', function() {
+    $view = new Template();
+    echo $view->render('views/home.html');
+});
+
+//run fat-free
+$f3->run();
